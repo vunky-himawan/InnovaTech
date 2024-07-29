710a8f8dfdd84f67a4547dab9d6070e9b1e9108f
Vunky Himawan <tingkerbeii44@gmail.com>
2024-07-29 12:54:20 +0700

    feat: add responsive for community list page

--- src/components/CommunityPageSection/CommunityList.tsx
+++ src/components/CommunityPageSection/CommunityList.tsx
@@ -9,7 +9,7 @@ const CommunityList = ({
   return (
     <>
       <section className="max-w-1800px h-fit mx-auto px-4 sm:px-6 lg:px-8 p-10">
-        <div className="grid grid-cols-4 gap-10">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-10">
           {data.map((community) => (
             <Card data={community.data} slug={community.slug} />
           ))}
